00c9eda11658fd93343230ffbdbb436c216b4069
채승지 <69447192+ChaeSeungJi@users.noreply.github.com>
2023-06-06 16:32:17 +0900

    Update starbucksCrawl.py

--- mathematical-logic/starbucksCrawl.py
+++ mathematical-logic/starbucksCrawl.py
@@ -3,41 +3,57 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from tabulate import tabulate
+import time
 
-class StarbucksCrawl:
+class starbucksCrawl:
     def __init__(self):
         self.driver_path = "./chromedriver.exe"
 
-    def get_truth_table(self, expression):
-        # Chrome 웹드라이버 실행 옵션 설정
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # 웹 브라우저 창을 띄우지 않고 실행하려면 추가합니다.
+        # 웹 브라우저 창을 띄우지 않고 실행하려면 추가합니다.
+        chrome_options.add_argument("--headless")
 
         # 크롬 웹드라이버 경로와 옵션을 설정하여 Service 객체 생성
         service = Service(self.driver_path)
 
         # Service 객체와 옵션을 사용하여 Chrome 웹드라이버 생성
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+
+       
+
+    def get_truth_table(self, expression):
+        # Chrome 웹드라이버 실행 옵션 설정
+
+        
+        wait = WebDriverWait(self.driver,10)
+
 
         # Truth Table 생성기 웹페이지 로드
-        driver.get("https://proofmood.mindconnect.cc/ko/TruthTable/tr_table.php")
+        self.driver.get(
+            "https://proofmood.mindconnect.cc/ko/TruthTable/tr_table.php")
 
         # 논리식 입력란에 논리식 입력
-        driver.execute_script("arguments[0].value = arguments[1]", driver.find_element(By.NAME, "fmla_str"), expression)
+        input_element = wait.until(EC.presence_of_element_located((By.NAME, "fmla_str")))
+        input_element.send_keys(expression)
+
 
         # Go 버튼 클릭
-        driver.execute_script("arguments[0].click()", driver.find_element(By.NAME, "go"))
+        go_button = wait.until(EC.element_to_be_clickable((By.NAME, "go")))
+        go_button.click()
 
         # Truth Table이 나타날 때까지 잠시 대기
-        driver.implicitly_wait(1)
+        wait.until(EC.presence_of_element_located((By.ID, "the_table")))
 
         # 웹페이지의 HTML 소스코드 가져오기
-        html = driver.page_source
+        html = self.driver.page_source
 
         # BeautifulSoup을 사용하여 HTML 파싱
         soup = BeautifulSoup(html, "html.parser")
+
 
         # Truth Table 데이터 추출
         table = soup.find("div", id="the_table")
@@ -58,19 +74,19 @@ class StarbucksCrawl:
             df = pd.DataFrame(data)
             df = df.drop(df.index[-1])
 
-        # 웹드라이버 종료
-        driver.quit()
-
         return df, target_column_index
 
-    def calculate_true_or_false(self, df, target_column_index):
+    def calculateTrueOrFalse(self, df, target_column_index):
         values = df.iloc[:, target_column_index].tolist()
         for v in values:
             if v == '0':
                 return "False"
         return "True"
 
-    def print_logic(self, input_string):
-        df, i = self.get_truth_table(input_string)
-        print(self.calculate_true_or_false(df, i))
+    def printLogic(self, inputString):
+        df, i = self.get_truth_table(inputString)
+        print(self.calculateTrueOrFalse(df, i))
         print(tabulate(df, headers='keys', tablefmt='psql', showindex=True))
+
+    def close(self):
+        self.driver.quit()
